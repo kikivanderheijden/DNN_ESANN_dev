5803aa1a135634497c6e9fa3843a5b105e711161
Heijden <Kiki.vanderHeijden@unimaas.nl>
2019-11-07 14:33:13 +0100

    .

--- TrainDNN.py
+++ TrainDNN.py
@@ -4,16 +4,25 @@ dir_fufiles = "/home/jovyan/DNN_ESANN_dev" # specify directory where function fi
 dir_mofiles = "/home/jovyan/DNN_ESANN_dev" # specify directory where model files are located 
 
 # import packages, libraries, functions to call them later
-import ImportAndPrepare_Data
+#import ImportAndPrepare_Data
 from pytictoc import TicToc
 t = TicToc()
-from keras.models import load_model
+#from keras.models import load_model
+from keras.models import model_from_json
 
 # load data
-labels_rand_train, labels_rand_test, an_l_rand_train, an_l_rand_test, an_r_rand_train, an_r_rand_test = ImportAndPrepare_Data.im_and_prep()
+#labels_rand_train, labels_rand_test, an_l_rand_train, an_l_rand_test, an_r_rand_train, an_r_rand_test = ImportAndPrepare_Data.im_and_prep()
+
+t.tic()
+json_file = open(dir_mofiles+'/DNN_model1.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.summary()
+t.toc("loading the model took")
 
 # load model
-t.tic()
-mymodel = load_model(dir_mofiles+"/DNN_model1.h5")
-mymodel.summary()
-t.toc("loading the model took ")
+#t.tic()
+#mymodel = load_model(dir_mofiles+"/DNN_model1.h5")
+#mymodel.summary()
+#t.toc("loading the model took ")
