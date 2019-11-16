724b142af0cae11c73e51f0c231d2363710455da
Heijden <Kiki.vanderHeijden@unimaas.nl>
2019-11-16 12:41:57 +0100

    2-191116

--- ModelEvaluation_TestScript.py
+++ ModelEvaluation_TestScript.py
@@ -21,9 +21,13 @@ import pickle
 os.chdir(dirscripts)
 from CustLoss_MSE import cust_mean_squared_error
 from ModelPredictions import generate_model_predictions
+from CustLoss_cosine_distance import cos_dist_2D # note that in this loss function, the axis of the MSE is set to 1
+from CustMet_cosine_distance import cos_distmet_2D
+
+
 
 # define name of current model
-modelname = "model7"
+modelname = "model8"
 
 # model parameters for evaluation
 sizebatches = 64
@@ -35,7 +39,7 @@ azimuthrange = np.arange(0,360,10)
 # Preparations
 #------------------------------------------------------------------------------
 # load model
-model = load_model(dirfiles+'/'+modelname+'.h5', custom_objects={"cust_mean_squared_error": cust_mean_squared_error})
+model = load_model(dirfiles+'/'+modelname+'_final.h5', custom_objects={"cust_mean_squared_error": cust_mean_squared_error, "cos_dist_2D": cos_dist_2D, "cos_distmet_2D": cos_distmet_2D})
 #model = load_model(dirfiles+'/'+modelname+'_final.h5', custom_objects={"cust_mean_squared_error": cust_mean_squared_error})
 
 # load history of the model
@@ -59,7 +63,7 @@ Y_test = labels_test
 #------------------------------------------------------------------------------
 
 # predict --> create predictions (set to 1) or load predictions (set to 0)
-newpredictions = 0
+newpredictions = 1
 if newpredictions == 1:
     predictions = generate_model_predictions(model, X_test, modelname, dirfiles)
 elif newpredictions == 0:
