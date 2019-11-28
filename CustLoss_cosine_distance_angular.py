d9306e2b12de964cbea4c535d5d60227b12e4bbf
Heijden <Kiki.vanderHeijden@unimaas.nl>
2019-11-28 11:15:16 +0100

    20191128

--- CustLoss_cosine_distance_angular.py
+++ CustLoss_cosine_distance_angular.py
@@ -16,7 +16,7 @@ def cos_dist_2D_angular(y_true,y_pred):
     #cos_sim = np.sum(y_true*y_pred, axis=1)/(np.sqrt(np.sum(np.square(y_true),axis=1))*np.sqrt(np.sum(np.square(y_pred),axis=1)))
     cos_sim = K.sum(y_true*y_pred, axis=1)/(K.sqrt(K.sum(K.square(y_true),axis=1))*K.sqrt(K.sum(K.square(y_pred),axis=1)))
     
-    cosine_distance_degrees = K.arccos(cos_sim)*180/K.pi
+    cosine_distance_degrees = K.acos(cos_sim)*180/K.pi
     
     # take the mean across all samples because you have to return one scalar
     return cosine_distance_degrees
--- CustMet_cosine_distance_angular.py
+++ CustMet_cosine_distance_angular.py
@@ -5,7 +5,7 @@ from tensorflow.keras import backend as K
 def cos_distmet_2D_angular(y_true, y_pred):
     cos_sim = K.sum(y_true*y_pred, axis=1)/(K.sqrt(K.sum(K.square(y_true),axis=1))*K.sqrt(K.sum(K.square(y_pred),axis=1)))
 
-    cosine_distance_degrees = K.arccos(cos_sim)*180/K.pi
+    cosine_distance_degrees = K.acos(cos_sim)*180/K.pi
         
     # take the mean across all samples because you have to return one scalar
     
